cd878521b77890821b3545aea76fa9d80b05fc49
Manish Baxi <baxi.manish@welue.com>
2016-03-01 21:04:06 +0530

    Added sample for question 35719697.

---
+++ 35719697/src/main/java/org/example/RegularConfig.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import java.util.List;
+
+@Configuration
+@PropertySource(value = "classpath:example.properties")
+public class RegularConfig
+{
+  @Value("#{'${names}'.split(',')}")
+  private List<String> names;
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer()
+  {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+
+  public List<String> getNames()
+  {
+    return names;
+  }
+}
---
+++ 35719697/src/test/java/org/example/RegularConfigTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@ContextConfiguration(classes = RegularConfig.class, loader = AnnotationConfigContextLoader.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RegularConfigTest
+{
+  @Autowired
+  private RegularConfig config;
+
+  @Test
+  public void testContextLoads()
+  {
+  }
+
+  @Test
+  public void testGetNames()
+  {
+    List<String> names = config.getNames();
+
+    assertNotNull(names);
+    assertFalse(names.isEmpty());
+    assertNotEquals(1, names.size());
+  }
+}
