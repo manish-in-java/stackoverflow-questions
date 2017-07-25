3c0f23121d9b60d20b6fc3173af55143d643b1b5
Manish Java <manish.in.java@gmail.com>
2017-07-25 17:15:50 +0530

    Corrected package for the annotation.

--- 45264794/src/test/java/org/example/data/RouterRepositoryTest.java
+++ 45264794/src/test/java/org/example/data/RouterRepositoryTest.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertNotNull;
 
