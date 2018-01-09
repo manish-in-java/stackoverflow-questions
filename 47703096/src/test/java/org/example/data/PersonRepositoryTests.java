fd4ec6fa6b4fb48ffeb1bf4d9a869599488dff4c
Manish Java <manish.in.java@gmail.com>
2018-01-09 09:36:11 +0530

    Added integration test to reproduce a problem with parameter parsing.

--- 47703096/src/test/java/org/example/data/PersonRepositoryTests.java
+++ 47703096/src/test/java/org/example/data/PersonRepositoryTests.java
@@ -1,12 +1,16 @@
 package org.example.data;
 
+import org.example.domain.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,6 +18,31 @@ public class PersonRepositoryTests
 {
   @Autowired
   private PersonRepository repository;
+
+  @Test
+  public void testFindByFirstName()
+  {
+    final String firstName = UUID.randomUUID().toString(), lastName = "001:1";
+
+    final Person person = new Person();
+    person.setFirstName(firstName);
+    person.setLastName(lastName);
+
+    repository.save(person);
+
+    final List<Person> results = repository.findByFirstName(firstName);
+
+    assertNotNull(results);
+    assertFalse(results.isEmpty());
+
+    results.forEach(person1 -> {
+      assertNotNull(person);
+      assertNotNull(person.getFirstName());
+      assertNotNull(person.getLastName());
+      assertEquals(firstName, person.getFirstName());
+      assertEquals(lastName, person.getLastName());
+    });
+  }
 
   @Test
   public void testFindByFirstNameAndLastName()
