37b07581edd4fec54711279e9088c1108a464758
Manish Java <manish.in.java@gmail.com>
2019-04-18 15:16:51 +0530

    Added sample for question 55252842.

---
+++ 55252842/README.md
@@ -0,0 +1,3 @@
+Sample application to check CSV support in Spring Boot.
+
+See https://stackoverflow.com/questions/55252842/csv-on-requestbody-using-spring.
--- 55252842/src/main/java/org/example/Controller/FooController.java
+++ 55252842/src/main/java/org/example/Controller/FooController.java
@@ -1,5 +1,18 @@
 package org.example.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.InputStream;
+
+@Controller
+@RequestMapping("/csv")
 public class FooController
 {
+  @PostMapping(consumes = "text/csv")
+  public void upload(@RequestBody InputStream request)
+  {
+  }
 }
